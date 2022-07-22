99012a2b716310f40dda41df41b2547d9a258654
jini <cathkid00@gmail.com>
2022-07-22 15:39:07 +0900

    로그아웃까지

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const dataj = fs.readFileSync("./database.json");
 const parseData = JSON.parse(dataj);
 const mysql = require('mysql');
 // 회원가입 / 로그인
-const session = require('express-session');  
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');     // 쿠키 사용
+const session = require('express-session');  
+const cookie = require('react-cookie');
 // const crypto = require('crypto');
 // const salt = require('salt');
-const cookieParser = require('cookie-parser');      // 쿠키 사용
 
 //데이터베이스 연결
 const connection = mysql.createConnection({
@@ -23,8 +24,47 @@ const connection = mysql.createConnection({
 })
 
 app.use(express.json()) //json형식의 데이터를 처리할수 있도록설정
-app.use(cors()) //브라우저의 다양한 사용을 위해 설정
+// app.use(cors()) //브라우저의 다양한 사용을 위해 설정
+//로그인 - 쿠키 사용
+//미들웨어 설정
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET","POST","DELETE","PUT"],
+    // methods: ["GET","POST"],
+    credentials: true
+}));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }))  // 회원가입 - 클라이언트로부터 값 받아오기
+//세션
+app.use(session({
+    key: "userId",                      //name of cookie
+    secret: "subscribe",                //중요
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60 * 60 * 24,      //24hours
+    },
+}))
+//쿠키 접근
+//ex>
+// cookie.save('userId',"test"
+//     ,{  
+//         path: '/',
+//         expires: 60 * 60 * 24,
+//     }
+// )
+function componentDidMount() {
+    const expires = new Date()
+    expires.setMinutes(expires.getMinutes() + 60)
+    cookie.save('userId',"test",
+    {
+        path: '/',
+        expires,
+    })
+    setTimeout(function() {
+        alert(cookie.load('userId'))
+    },1000)
+};
 
 //1. 전체상품
 app.get('/dresses', async (req, res)=>{
@@ -33,7 +73,6 @@ app.get('/dresses', async (req, res)=>{
         (err, rows, fields)=>{
             res.send(rows);
             console.log(err);
-            console.log(fields);
         }
     )
 })
@@ -44,56 +83,61 @@ app.get('/dress/:id', async (req, res)=>{
         `select * from dress_table where id=${params.id}`,
         (err, rows, fields)=>{
             res.send(rows);
-            console.log(fields);
         }
     )
 })
-//3. 회원가입 - 클라이언트에서 값을 받아오기
+//3. 회원가입 - 클라이언트에서 값을 받아오기  --> bcrypt 설치
 // db(데이터베이스)에 회원정보 업로드되게 하기
 // const db = require('./database.json')
 // 비밀번호 보안 - bcrypt 사용(bcrypt에서 제공하는 hash 함수를 사용)
-// const bcrypt = require('bcrypt')
-// const saltRounds = 10       // saltRounds : 암호화를 몇번 진행할 것인지
+const bcrypt = require('bcrypt');
+const saltRounds = 10       // saltRounds : 암호화를 몇번 진행할 것인지
 
 app.post('/register', async (req,res,next) =>{
 	const param = [req.body.c_id, req.body.c_password, req.body.c_name, req.body.c_gender, req.body.c_phone, req.body.c_phone2, req.body.c_phone3, req.body.c_add, req.body.c_adddetail, req.body.c_email] 
         
         // *비밀번호 보안- hash
-        // bcrypt.hash(param[1], saltRounds, (error , hash)=>{
-        // param[1] = hash
-        // // *보안2
-        let password = req.body.c_password;
-        // const hashPassword = crypto.createHash('sha512').update(password).digest('hex');
-        const hashPassword = crypto.createHash('sha512').update(password + salt).digest('hex');
-        // // *id 중복처리하가위한 쿼리문  --> 클라이언트에서 추가하기!
-        // **table의 column인 userid가 primary_key라서 어짜피 중복이 안됨 -> 안해줘도 됨!**
-        // const query = "SELECT userid FROM member where userid=?";
-        // let user_id = req.body.c_id;
-        // connection.query(query, [user_id], function(error, rows, fields){
-        //     console.log(rows);
-        //     let checkId = new Object();
-        //     checkId.tf = false; // 이 아이디를 사용할 것인지
-        //     if(rows[0] === undefined){  // 중복되는게 없으면
-        //         checkId.tf = true;  //없음 -> 사용가능
-        //         res.send(checkId);  // 클라이언트로 다시 보낸다 checkID객체를
-        //         console.log("성공");
-        //         // 쿼리문 작성
-        //         // console.log("여기에요!!!!");
-        //         console.log(param);
-        //         connection.query('INSERT INTO member(`userid`,`pw`,`name`,`gender`,`phone`,`phone2`,`phone3`,`add`,`adddetail`,`email`) VALUES (?,?,?,?,?,?,?,?,?,?)' , 
-        //         param , (err, row) =>{
-        //             if(err) { console.log("가입안됨");} 
-        //             else {
-        //                 console.log("가입완료")
-        //             }
-        //         }) 
-        //     }
-        //     else {
-        //         checkId.tf = false;    // 중복될 경우 -> 사용X
-        //         res.send(checkId);
-        //         console.log("중복ID");
-        //     }
-        // })
+        //param[1] --> req.body.c_password
+        bcrypt.hash(param[1], saltRounds, (err , hash) => {
+            // param[1] = hash
+            if (err) {
+                console.log(err);
+            }
+
+
+            // // *보안2
+            // let password = req.body.c_password;
+            // const hashPassword = crypto.createHash('sha512').update(password).digest('hex');
+            // const hashPassword = crypto.createHash('sha512').update(password + salt).digest('hex');
+            // // *id 중복처리하가위한 쿼리문  --> 클라이언트에서 추가하기!
+            // **table의 column인 userid가 primary_key라서 어짜피 중복이 안됨 -> 안해줘도 됨!**
+            // const query = "SELECT userid FROM member where userid=?";
+            // let user_id = req.body.c_id;
+            // connection.query(query, [user_id], function(error, rows, fields){
+            //     console.log(rows);
+            //     let checkId = new Object();
+            //     checkId.tf = false; // 이 아이디를 사용할 것인지
+            //     if(rows[0] === undefined){  // 중복되는게 없으면
+            //         checkId.tf = true;  //없음 -> 사용가능
+            //         res.send(checkId);  // 클라이언트로 다시 보낸다 checkID객체를
+            //         console.log("성공");
+            //         // 쿼리문 작성
+            //         // console.log("여기에요!!!!");
+            //         console.log(param);
+            //         connection.query('INSERT INTO member(`userid`,`pw`,`name`,`gender`,`phone`,`phone2`,`phone3`,`add`,`adddetail`,`email`) VALUES (?,?,?,?,?,?,?,?,?,?)' , 
+            //         param , (err, row) =>{
+            //             if(err) { console.log("가입안됨");} 
+            //             else {
+            //                 console.log("가입완료")
+            //             }
+            //         }) 
+            //     }
+            //     else {
+            //         checkId.tf = false;    // 중복될 경우 -> 사용X
+            //         res.send(checkId);
+            //         console.log("중복ID");
+            //     }
+            // })
             // 쿼리문 작성
             // console.log("여기에요!!!!");
             console.log(param);
@@ -104,208 +148,88 @@ app.post('/register', async (req,res,next) =>{
                     console.log("가입완료")
                 }
             }) 
-    // })
+        })
     res.end()
 })
 //4. 로그인
 app.post('/login', (req, res) => {
-    const username = req.body.username;
+    const username = req.body.username;     // req.body.username의 username은 client에서 내가 값을 넘겨줄 때, 변수와? 같아야한다(username: userName 에서 여기 앞의 username!!)
     const userpassword = req.body.userpassword;
     console.log(req.body)
-    connection.query('SELECT * FROM member WHERE userid = ? AND pw = ?',
-    [username, userpassword],
+    // connection.query('SELECT * FROM member WHERE userid = ? AND pw = ?',
+    // [username, userpassword],
+    // (err,result)=>{
+    //     if(err) {
+    //         console.log(err);
+    //         res.send({ err: err });
+    //     }
+    //     if(result.length > 0) {     // 아이디 비밀번호가 맞다면
+    //         res.send(result);
+    //         console.log('성공')
+    //     } else {
+    //         res.send({ message: "Wrong userid/password combination!" });
+    //     }
+    // });
+    //bcrypt 비밀번호 보안 후 수정
+    connection.query('SELECT * FROM member WHERE userid = ?;',        
+    username,
     (err,result)=>{
+        console.log(result);
+        console.log(req.body);
         if(err) {
             console.log(err);
             res.send({ err: err });
         }
-        if(result) {
-            res.send(result);
-            console.log('성공')
+        if(result.length > 0) {     // 아이디가 맞다면
+            bcrypt.compare(userpassword, result[0].pw, (error, response) => {
+                if(userpassword == result[0].pw) {
+                    req.session.user = result;
+                    console.log(req.session.user);
+                    res.send(result);
+//                     req.session.loginData = rows;
+// //                         req.session.save(error => {
+// //                             if(error) console.log(error)
+// //                         })
+// //                         res.json({message: 'success'})
+                } else {
+                    res.send({ message: "Wrong userid/password combination!" });
+                }
+            })
         } else {
-            res.send({ message: "Wrong userid/password combination!" });
+            res.send({ message: "User doesn`t exist!" });
         }
     });
 });
 
-
-
-
-
-
-// app.get('/Login', (req, res) => {
-//     res.send({data: 'data'})        // 임시로 값 넣어주기
-// })
-// //
-// const util = require('util');
-// app.post('/onLogin', (req, res) => {
-//     console.log(`= = = > req : ${util.inspect(req)} `)
-//     //query: 'userid=ffff&pw=ggggg' 로그인페이지에 작성한 아이디랑 비번이 서버터미널 쿼리에 잘 담김
-//     // user_id, user_pw 변수로 선언
-//     const user_id = req.query.userid;
-//     const user_pw = req.query.pw;
-//     // 입력된 id 와 동일한 id 가 mysql 에 있는 지 확인
-//     const sql1 = 'SELECT COUNT(*) AS result FROM member WHERE userid = ? AND pw = ?'
-//     const params = [user_id, user_pw]
-//     connection.query(sql1, params , function(err, data){
-//         if(data[0].result < 1){
-//             res.send({'msg': '입력하신 id와 pw가 일치하지 않습니다.'})
-//         }else {
-//             res.send(data[0]);
-//         }
-//     })
-//     // connection.query(sql1, user_id, (err, data) => {
-//     //     if(!err) {
-//     //         // 결과값이 1보다 작다면(동일한 id가 없다면)(id가 없음)
-//     //         if(data[0].result < 1){
-//     //             res.send({ 'msg' : '입력하신 id가 일치하지 않습니다.' })
-//     //         } else {    // 동일한 id가 있으면 비밀번호 일치 확인
-//     //             const sql2 = `SELECT 
-//     //             CASE (SELECT COUNT(*) FROM member WHERE userid = ? AND pw = ?)
-//     //                 WHEN '0' THEN NULL
-//     //                 ELSE (SELECT userid FROM member WHERE userid = ? AND pw = ?)
-//     //             END AS userId
-//     //             , CASE (SELECT COUNT(*) FROM member WHERE userid = ? AND pw = ?)
-//     //                 WHEN '0' THEN NULL
-//     //                 ELSE (SELECT pw FROM member WHERE userid = ? AND pw = ?)
-//     //             END AS userPw`;
-//     //             // sql 란에 필요한 parameter 값을 순서대로 기재
-//     //             const params = [user_id, user_pw, user_id, user_pw, user_id, user_pw, user_id, user_pw]
-//     //             db.query(sql2, params, (err, data) => {
-//     //                 if(!err) {
-//     //                     res.send(data[0])
-//     //                 } else {
-//     //                     res.send(err)
-//     //                 }
-//     //             })
-//     //         }
-//     //     } else {
-//     //         res.send(err);
-//     //     }
-//     // })
-// })
+app.get('/login', (req, res)=>{
+    if (req.session.user) {
+        res.send({loggedIn: true, user: req.session.user})
+    } else {
+        res.send({loggedIn: false })
+    }
+})
 //5. 로그아웃
-
-
-
-
-// app.post('/', function (req, res, next){
-//     let id = req.body.c_id;
-//     let pw = req.body.c_password;
-     
-//     // connection.query('Select salt, pw From member where userid="id";',
-//     connection.query('Select pw From member where userid="id";',
-//         function(err, rows){
-//             if(err) console.log(err);
-//             else {
-//                 if(rows.length == 0){
-//                     console.log("아이디 틀림")
-//                     res.redirect('/Login')
-//                 }
-//                 else {
-//                     // let salt = rows[0].salt;
-//                     let password = rows[0].pw;
-//                     // console.log(rows[0].pw);
-//                     // 비밀번호 보안
-//                     // const hashPassword = crypto.createHash('sha512').update(pw + salt).digest('hex');
-//                     const hashPassword = crypto.createHash('sha512').update(pw).digest('hex');
-//                     if(password === hashPassword){  //로그인성공
-//                         console.log("로그인성공");
-//                         res.cookie("user", id, {
-//                             expires: new Date(Date.now() + 900000),     //로그인지속시간: 로그인이 성공했을 시에 cookieparser를 사용하여 현재시간으로부터 900000ms 동안 지속되는 쿠키를 생성
-//                             httpOnly: true
-//                         });
-//                         res.redirect('/')
-//                     }
-//                     else {
-//                         console.log("로그인 실패 비밀번호 틀림")
-//                         res.redirect('/Login')
-//                     }
-//                 }
-//             }
-//         }
-//     )
+// app.get('/logout', (req, res) => {
+//     res.clearCookie("userId");       //쿠키이름 
+//     res.redirect("/");
+//     console.log("로그아웃 되었습니다!!!");
 // })
-// //
-// app.post('/Login', (req, res, next) => {
-//     param = [ req.body.c_id, req.body.c_pw]
-//     connection.query('SELECT * FROM member WHERE userid=?', param[0], 
-//     (err, rows)=>{
-//         if(err) console.log(err);
-//         if(rows.length > 0 ){   //ID 존재
-//             // console.log("ID가 존재합니다.")
-//             //비밀번호가 맞는지 - bcrypt 에서 지원하는 compare함수 이용
-//             bcrypt.compare(param[1], rows[0].pw, 
-//                 (err, result)=>{
-//                     if(result){     // 성공
-//                         req.session.loginData = rows;
-//                         req.session.save(error => {
-//                             if(error) console.log(error)
-//                         })
-//                         res.json({message: 'success'})
-//                     }else {         // 실패
-//                         res.json({message: 'fail'})
-//                     }
-//                 })
+app.post('/logout', (req,res) =>{
+    if(req.session.user){
+        req.session.destroy(error => {if(error) console.log(error) })
+        res.redirect('/');
+    }else{
+        console.log("로그인해주세요!!!");
+    }
+})
 
-//         }else {
-//             console.log("ID가 존재하지 않습니다.")
-//         }
-//     })
-//     res.end();
-// })
-// // 로그아웃
-// app.post('/Logout', (req,res) =>{
-//     if(req.session.loginData){
-//         req.session.destroy(error => {if(error) console.log(error) })
-//     }else{
-//         /* 세션정보가 없을때 */
-//     }
-// })
-// // 미들웨어 설정
-// app.use(cors({
-//     origin: true,       // origin : 흔히 알고있는 도메인의 형태를 띄는데 이것들을 모두 허용해준다는 뜻으로 true를 작성한다. (true나 특정 url을 적어두기)
-//     credentials: true   // 교차 출처 리소스 공유 (CORS)
-// }))
-// app.use(cookieParser());    // 쿠키 추출
-// app.use(
-//     session({
-//         key: "loginData",
-//         secret: "testSecret",
-//         resave: false,
-//         saveUninitialized: false,
-//         cookie: {
-//             expires: 60 * 60 * 24       // 쿠키 지속시간
-//         },
-//     })
-// )
-// // // Auto Check
-// // app.get('/loginCheck', (req,res) =>{
-// //     if(req.session.loginData){
-// //         res.send({loggedIn : true, loginData: req.session.loginData})
-// //     }else{
-// //         res.send({loggedIn : false})
-// //     }
-// // })
-// // 로그인 로그아웃
-// // app.post('/Login', (req, res)=>{
-// //     if(){       //로그인 성공
-// //         req.session.loginData = 
-// //         req.session.save(error => {
-// //             if(error) console.log(error)
-// //         })
-// //         res.json({message: 'success'})
-// //     }else { //로그인실패
-// //         res.json({message: 'fail'})
-// //     }
-// // })
 
 // ** 관리자 권한으로 로그인시에만 등록/삭제/수정 되게 나중에 할거임..!
 //6. 상품등록
 app.post('/uploadDress', async (req, res) => {
     const { c_name, c_price, c_size1, c_size2, c_size3, c_type, c_desc1, c_desc2, c_pic1, c_pic2, c_pic3 } = req.body;
     console.log(req.body)
-    connection.query("INSERT INTO dress_table(`name`,`price`,`size1`,`size2`,`size3`,`type`,`desc`,`desc2`,`imgsrc`,`imgsrc2`,`imgsrc3`) values(?,?,?,?,?,?,?,?,?,?,?)",
+    connection.query("INSERT INTO dress_table(`name`,`price`,`size1`,`size2`,`size3`,`type`,`desc1`,`desc2`,`imgsrc`,`imgsrc2`,`imgsrc3`) values(?,?,?,?,?,?,?,?,?,?,?)",
     [c_name,c_price,c_size1,c_size2,c_size3,c_type,c_desc1,c_desc2,c_pic1,c_pic2,c_pic3],
     (err, result, fields)=>{
         if(result){
@@ -326,6 +250,19 @@ app.delete('/delDress/:id', async (req, res)=>{
     })    
 })
 //8. 상품수정
+app.put('/editDress/:id', async (req, res)=>{
+    const params = req.params;
+    const { c_name, c_price, c_size1, c_size2, c_size3, c_type, c_desc1, c_desc2, c_pic1, c_pic2, c_pic3 } = req.body;
+    console.log(req.body)
+    connection.query(`UPDATE dress_table SET name='${c_name}', price=${c_price}, size1=${c_size1}, size2=${c_size2}, size3=${c_size3}, type='${c_type}', desc1='${c_desc1}', desc2='${c_desc2}', imgsrc='${c_pic1}', imgsrc2='${c_pic2}', imgsrc3='${c_pic3}' where id=${params.id}`,
+    (err, result, fields)=>{
+        if(err) {
+            console.log("에러발생!!");
+            console.log(err);
+        }
+        res.send(result);
+    })
+})
 
 // 서버실행
 app.listen(port, () => {
